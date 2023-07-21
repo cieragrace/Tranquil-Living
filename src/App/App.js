b85efe516b99231835cafe2bbbed02b1b74011e5
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-07-21 13:23:48 -0600

    Style Home image

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from '../Header/Header';
+import Home from '../Home/Home';
 
 const App = () => {
   return(
     <div>
       <Header />
+      <Home />
     </div>
   )
 }
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,7 +2,11 @@ import './Home.css'
 
 const Home = () => {
   return(
-    <div className='home-container'></div>
+    <div className='home-container'>
+      <div className='home-image'></div>
+      <h3>Clean  |  Inspired  |  Peaceful  |  Fresh  |  Serene</h3>
+      <h4>You too can have a home that feels like your own oasis</h4>
+    </div>
   )
 }
 
