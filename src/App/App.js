41d49aefe872dcb9cdcc0783419ed122fbb03b37
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-07-21 14:17:29 -0600

    Style cleaning list section

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from '../Header/Header';
 import Home from '../Home/Home';
+import CleaningList from '../CleaningList/CleaningList';
 
 const App = () => {
   return(
     <div className='app-container'>
       <Header />
       <Home />
+      <CleaningList />
     </div>
   )
 }
