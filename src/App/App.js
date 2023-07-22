84f7a4dd0296abc37b1ec70b23260d82e41c8223
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-07-22 09:00:55 -0600

    Get data from API in state

--- src/App/App.js
+++ src/App/App.js
@@ -2,8 +2,21 @@ import './App.css';
 import Header from '../Header/Header';
 import Home from '../Home/Home';
 import CleaningList from '../CleaningList/CleaningList';
+import { useState, useEffect } from 'react';
+import getAPIData from '../apiCalls';
 
 const App = () => {
+const [cleaningLists, setCleaningLists] = useState([])
+
+useEffect(() => {
+  const getCleaningLists = () => {
+    getAPIData()
+    .then((data) => setCleaningLists(data.tasks))
+    .catch((error) => console.log("error"))
+  }
+  getCleaningLists()
+}, []) 
+
   return(
     <div className='app-container'>
       <Header />
