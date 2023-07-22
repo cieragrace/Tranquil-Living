4a6e9abc6463fef851b7bba01713dcd936cd48c3
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-07-22 07:58:54 -0600

    Adjust to change of layout on list container

--- src/CleaningList/CleaningList.js
+++ src/CleaningList/CleaningList.js
@@ -1,12 +1,16 @@
 import './CleaningList.css'
-
+import { Link } from 'react-router-dom'
 const CleaningList = () => {
   return(
     <div className='list-section'>
       <section className='list-container'>
-        <h4 className='title'>Today's Clean Home Checklist</h4>
+        <h4 className='title'>My Clean Home Checklists</h4>
         <div className='line'></div>
-        <h3 className='daily'>Daily</h3>
+        <Link className='daily link'>Daily</Link>
+        <Link className='weekly link'>Weekly</Link>
+        <Link className='monthly link'>Monthly</Link>
+        <Link className='seasonal link'>Seasonal</Link>
+        <Link className='annual link'>Annual</Link>
       </section>
     </div>
   )
